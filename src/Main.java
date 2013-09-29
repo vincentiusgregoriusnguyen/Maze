5555fced578592b5cae35a206b574ef4e487e54a
vincentiusgregoriusnguyen <zcabh35@live.ucl.ac.uk>
2013-09-29 11:17:29 +0100

    Creating list of directions for travel

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 // based on http://stackoverflow.com/questions/9191428/maze-solving-algorithm-in-c/9192067#9192067
 
 
@@ -6,8 +8,19 @@ public class Main {
 	static int Height = 9;
     static int Width = 9;
     
-    static Coordinate Start = new Coordinate(1,0);
-    static Coordinate End = new Coordinate(7,8);
+    static Coordinate Start = new Coordinate(7,8);
+    static Coordinate End = new Coordinate(1,0);
+    
+    static char CurrentDirection = 'n';
+    
+    static int previousX = 0;
+    static int previousY = 0;
+    
+    static int currentX = 7;
+    static int currentY = 8;
+    
+    static ArrayList<String> instructions = new ArrayList<String>();
+ 
 	
 	static char Maze[][] ={
 			{'#',' ','#','#','#','#','#','#','#'},
@@ -25,6 +38,77 @@ public class Main {
 	static char Free = ' ';
 	static char Path = '*';
 	
+	public static void findpath(){
+			if(CurrentDirection == 'n'){
+				if(currentY - 1 >= 0 && (Maze[currentY - 1][currentX] == '*') || (Maze[currentY - 1][currentX] == 'E')){ 
+					currentY = currentY - 1;
+					instructions.add("Forward");
+				}
+				if(currentX - 1 >= 0 && (Maze[currentY][currentX - 1] == '*') || (Maze[currentY][currentX - 1] == 'E')){
+					currentX = currentX - 1;
+					instructions.add("Left");
+					CurrentDirection = 'w';
+				}
+				if(currentX + 1 < Width && (Maze[currentY][currentX + 1] == '*') || (Maze[currentY][currentX + 1] == 'E')){
+					currentX = currentX + 1;
+					instructions.add("Right");
+					CurrentDirection = 'e';
+				}	
+			}
+			
+			if(CurrentDirection == 'e'){
+				if(currentX + 1 < Width && (Maze[currentY][currentX - 1] == '*') || (Maze[currentY][currentX - 1] == 'E')){
+					currentX = currentX - 1;
+					instructions.add("Forward");
+				}
+				if(currentY - 1 >= 0 && (Maze[currentY - 1][currentX] == '*') || (Maze[currentY - 1][currentX] == 'E')){
+					currentY = currentY - 1;
+					instructions.add("Left");
+					CurrentDirection = 'n';
+				}
+				if(currentY + 1 < Height && (Maze[currentY + 1][currentX] == '*') || (Maze[currentY + 1][currentX] == 'E')){
+					currentY = currentY + 1;
+					instructions.add("Right");
+					CurrentDirection = 's';
+				}
+			}
+			
+			
+			if(CurrentDirection == 's'){
+				if(currentY + 1 < Height && (Maze[currentY + 1][currentX] == '*') || (Maze[currentY + 1][currentX] == 'E')){
+					currentY = currentY + 1;
+					instructions.add("Forward");
+				}
+				if(currentX + 1 < Width && (Maze[currentY][currentX + 1] == '*') || (Maze[currentY][currentX + 1] == 'E')){
+					currentX = currentX + 1;
+					instructions.add("Left");
+					CurrentDirection = 'e';
+				}
+				if(currentX - 1 >= 0 && (Maze[currentY][currentX - 1] == '*') || (Maze[currentY][currentX - 1] == 'E')){
+					currentX = currentX - 1;
+					instructions.add("Right");
+					CurrentDirection = 'w';
+				}
+			}
+			
+			if(CurrentDirection == 'w'){
+				if(currentY - 1 >= 0 && (Maze[currentY - 1][currentX] == '*') || (Maze[currentY - 1][currentX] == 'E')){
+					currentY = currentY - 1;
+					instructions.add("Right");
+					CurrentDirection = 'n';
+				}
+				if(currentX - 1 >= 0 && (Maze[currentY][currentX - 1] == '*') || (Maze[currentY][currentX - 1] == 'E')){
+					currentX = currentX - 1;
+					instructions.add("Forward");
+				}
+				if(currentY + 1 < Height && (Maze[currentY+1][currentX] == '*') || (Maze[currentY+1][currentX] == 'E')){
+					currentY = currentY + 1;
+					instructions.add("Left");
+					CurrentDirection = 's';
+				}	
+		}
+	} 
+	
 	public static void printmaze(){
 		for(int i = 0; i < Maze.length; i++){
 			for(int j = 0; j < Maze.length; j++){
@@ -35,7 +119,13 @@ public class Main {
 	}
 	
 	public static boolean solve(int x, int y){
+		if(x == Start.getX() && y == Start.getY()){
+			Maze[y][x] = 'S';
+		} else if (x == End.getX() && y == End.getY()){
+			Maze[y][x] = 'E';
+		}else{
 		Maze[y][x] = Path;
+		}
 		
 		if(x == End.getX() && y == End.getY()){
 			return true;
@@ -67,6 +157,12 @@ public class Main {
 		if(solve(Start.getX(), Start.getY())){
 			System.out.println("Solution");
 			printmaze();
+			
+			findpath();
+			
+			for(String x: instructions){
+				System.out.println(x);
+			}
 		}
 		else{
 			System.out.println("no solution");
